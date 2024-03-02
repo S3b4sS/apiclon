8f21e338ed6cb7bb61b6b83029bbffb3629043c9
ssuf <ssuf2002@gmail.com>
2024-03-02 12:00:14 -0400

    swagger

--- django_project/urls.py
+++ django_project/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path("api/dj-rest-auth/",include("dj_rest_auth.urls")),
     path("api/dj-rest-auth/registration/",include("dj_rest_auth.registration.urls")),
     path("api/schema/", SpectacularAPIView.as_view(),name= "schema"),
-    path("api/Schema/Redoc/", SpectacularAPIView.as_view(url_name = "schema"), name= "redoc"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name = "schema"), name= "redoc"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name = "schema"), name= "swagger-ui"),
 ]
