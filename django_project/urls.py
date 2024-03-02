d7b3d77c0b172ec729e69ab66a9244eb819b187b
ssuf <ssuf2002@gmail.com>
2024-03-02 11:21:56 -0400

    ekisde

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
